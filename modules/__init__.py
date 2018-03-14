5ff59d152f7351cc1f45672b66c83e5b787ecf32
Werewolfas <werewolfas@gmail.com>
2018-03-14 16:51:41 +0200

    Updated requirements. Added versioning

---
+++ modules/__init__.py
@@ -0,0 +1,8 @@
+from .dbase import DBase
+from .destiny_api import DestinyApi
+from .image_magic import ImageMagic
+from .telegram_bot import TelegramBot
+from .translate import Translate
+
+title = 'destiny-telegram-bot'
+__version__ = '1.0.0'
